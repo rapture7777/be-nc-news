0bf6ac65af8f7de832e27020f150ad9afd991a02
Adam's MacBook Pro <macbook@Macbooks-MacBook-Pro.local>
2019-12-13 14:06:37 +0000

    Completed util funcs and testing, completed seed.js

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -8,6 +8,26 @@ exports.formatDates = list => {
   });
 };
 
-exports.makeRefObj = list => {};
+exports.makeRefObj = list => {
+  if (!list.length) return {};
+  else {
+    let refObj = {};
+    list.forEach(function(obj) {
+      refObj[obj.title] = obj.article_id;
+    });
+    return refObj;
+  }
+};
 
-exports.formatComments = (comments, articleRef) => {};
+exports.formatComments = (comments, articleRef) => {
+  if (!comments.length) return [];
+  return comments.map(function(comment) {
+    newComment = { ...comment };
+    newComment.author = newComment.created_by;
+    delete newComment.created_by;
+    newComment.article_id = articleRef[newComment.belongs_to];
+    delete newComment.belongs_to;
+    newComment.created_at = new Date(newComment.created_at);
+    return newComment;
+  });
+};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -56,8 +56,217 @@ describe('formatDates', () => {
       }
     ]);
   });
+  it('does not mutate the original objects', () => {
+    let time = new Date().getTime();
+    let objArr = [
+      {
+        id: 1,
+        created_at: time
+      },
+      {
+        id: 2,
+        created_at: time
+      },
+      {
+        id: 3,
+        created_at: time
+      }
+    ];
+    formatDates(objArr);
+    expect(objArr).deep.equal([
+      {
+        id: 1,
+        created_at: time
+      },
+      {
+        id: 2,
+        created_at: time
+      },
+      {
+        id: 3,
+        created_at: time
+      }
+    ]);
+  });
 });
 
-describe('makeRefObj', () => {});
+describe('makeRefObj', () => {
+  it('takes an array containing an empty object and returns a new object', () => {
+    let objArr = [{}];
+    expect(makeRefObj(objArr[0])).to.not.equal(objArr[0]);
+  });
+  it('takes an array with one object containing article_id and title keys and returns a new object with the key of titles value and the value of article_ids value', () => {
+    let objArr = [{ article_id: 1, title: 'A' }];
+    expect(makeRefObj(objArr)).to.deep.equal({ A: 1 });
+  });
+  it('takes an array with mutliple objects and returns expected result', () => {
+    let objArr = [
+      { article_id: 1, title: 'A' },
+      { article_id: 2, title: 'B' },
+      { article_id: 3, title: 'C' }
+    ];
+    expect(makeRefObj(objArr)).to.deep.equal({ A: 1, B: 2, C: 3 });
+  });
+  it('does not mutate the original objects', () => {
+    let objArr = [
+      { article_id: 1, title: 'A' },
+      { article_id: 2, title: 'B' },
+      { article_id: 3, title: 'C' }
+    ];
+    makeRefObj(objArr);
+    expect(objArr).to.deep.equal([
+      { article_id: 1, title: 'A' },
+      { article_id: 2, title: 'B' },
+      { article_id: 3, title: 'C' }
+    ]);
+  });
+});
 
-describe('formatComments', () => {});
+describe('formatComments', () => {
+  it('when passed an empty array returns a new array', () => {
+    let array = [];
+    expect(formatComments(array)).to.not.equal(array);
+  });
+  it('when passed an array containing one comment object and a reference obj returns a new array of the comment correctly formatted', () => {
+    let oldTime = new Date().getTime();
+    let newTime = new Date(oldTime);
+    let objArray = [
+      {
+        comment_id: 1,
+        created_by: 'Adam',
+        belongs_to: 'A',
+        created_at: oldTime,
+        body: 'Hello'
+      }
+    ];
+    let refObj = {
+      A: 1,
+      B: 2,
+      C: 3
+    };
+    expect(formatComments(objArray, refObj)).to.deep.equal([
+      {
+        comment_id: 1,
+        author: 'Adam',
+        article_id: 1,
+        created_at: newTime,
+        body: 'Hello'
+      }
+    ]);
+  });
+  it('when passed an array containing multiple comment objects and a reference obj returns the correct result', () => {
+    let oldTime = new Date().getTime();
+    let newTime = new Date(oldTime);
+    let objArray = [
+      {
+        comment_id: 1,
+        created_by: 'Adam',
+        belongs_to: 'A',
+        created_at: oldTime,
+        body: 'Hello'
+      },
+      {
+        comment_id: 2,
+        created_by: 'Terry',
+        belongs_to: 'B',
+        created_at: oldTime,
+        body: 'New'
+      },
+      {
+        comment_id: 3,
+        created_by: 'Andy',
+        belongs_to: 'C',
+        created_at: oldTime,
+        body: 'World'
+      }
+    ];
+    let refObj = {
+      A: 1,
+      B: 2,
+      C: 3
+    };
+    expect(formatComments(objArray, refObj)).to.deep.equal([
+      {
+        comment_id: 1,
+        author: 'Adam',
+        article_id: 1,
+        created_at: newTime,
+        body: 'Hello'
+      },
+      {
+        comment_id: 2,
+        author: 'Terry',
+        article_id: 2,
+        created_at: newTime,
+        body: 'New'
+      },
+      {
+        comment_id: 3,
+        author: 'Andy',
+        article_id: 3,
+        created_at: newTime,
+        body: 'World'
+      }
+    ]);
+  });
+  it('doesnt mutate the original objects', () => {
+    let oldTime = new Date().getTime();
+    let objArray = [
+      {
+        comment_id: 1,
+        created_by: 'Adam',
+        belongs_to: 'A',
+        created_at: oldTime,
+        body: 'Hello'
+      },
+      {
+        comment_id: 2,
+        created_by: 'Terry',
+        belongs_to: 'B',
+        created_at: oldTime,
+        body: 'New'
+      },
+      {
+        comment_id: 3,
+        created_by: 'Andy',
+        belongs_to: 'C',
+        created_at: oldTime,
+        body: 'World'
+      }
+    ];
+    let refObj = {
+      A: 1,
+      B: 2,
+      C: 3
+    };
+    formatComments(objArray, refObj);
+    expect(objArray).to.deep.equal([
+      {
+        comment_id: 1,
+        created_by: 'Adam',
+        belongs_to: 'A',
+        created_at: oldTime,
+        body: 'Hello'
+      },
+      {
+        comment_id: 2,
+        created_by: 'Terry',
+        belongs_to: 'B',
+        created_at: oldTime,
+        body: 'New'
+      },
+      {
+        comment_id: 3,
+        created_by: 'Andy',
+        belongs_to: 'C',
+        created_at: oldTime,
+        body: 'World'
+      }
+    ]);
+    expect(refObj).to.deep.equal({
+      A: 1,
+      B: 2,
+      C: 3
+    });
+  });
+});
